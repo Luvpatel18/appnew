f60bcbaec5858478096a64766c898536bdc28dd3
Luvpatel18 <imluvpatel18@gmail.com>
2024-05-26 14:19:53 -0700

    Initial Commit

---
+++ AppNew/ViewController.swift
@@ -0,0 +1,37 @@
+
+//
+//  ViewController.swift
+//  AppNew
+//
+//  Created by user224471 on 5/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var FirstName: UITextField!
+    
+    @IBOutlet weak var LastName: UITextField!
+    
+    @IBOutlet weak var Country: UITextField!
+    
+    @IBOutlet weak var Age: UITextField!
+    
+    
+    @IBAction func Add(_ sender: Any) {
+    }
+    
+    @IBAction func Submit(_ sender: Any) {
+    }
+    
+    @IBAction func Clear(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
